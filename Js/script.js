744cc5229a5a2ffb44bb5f908df167c0a4babd8e
alessiam1909 <ale.menon.vr@gmail.com>
2022-11-24 14:52:33 +0100

    Concludo variabile colore preferito e numero 21

--- Js/script.js
+++ Js/script.js
@@ -5,3 +5,19 @@ console.log(nomeUtente);
 // Creo la variabile per inserire il cognome dell'utente
 let cognome = prompt("Inserisci il tuo cognome");
 console.log(cognome);
+
+// Creo la variabile per inserire il colore preferito
+let colorePreferito = prompt("Inserisci il tuo colore preferito");
+console.log(colorePreferito);
+
+// Creo la variabile che inserisca automaticamente il numero 21
+let numero = 21;
+console.log(numero);
+
+// Creo la variabile che contenga la somma delle variabili sopra citate
+let password = `${nomeUtente} ${cognome} ${colorePreferito} ${numero}`;
+console.log(password);
+
+// Provo la concatenazione con +
+// let password = nomeUtente +" "+ cognome +" "+ colorePreferito +" "+ numero;
+// console.log(password);
