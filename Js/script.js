27078a0d2429acf5958d504fe0bccc3e21a3e486
alessiam1909 <ale.menon.vr@gmail.com>
2022-11-24 14:41:31 +0100

    creazione variabili per inserire nome e cognome

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+// Creo la variabile per inserire il nome utente
+let nomeUtente = prompt("Inserisci il tuo nome");
+console.log(nomeUtente);
+
+// Creo la variabile per inserire il cognome dell'utente
+let cognome = prompt("Inserisci il tuo cognome");
+console.log(cognome);
